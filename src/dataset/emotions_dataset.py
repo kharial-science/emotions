7bf1bde3e7cfadf7ea78caff7d0e4b30010f2ad6
Kharoh <Gaulu03@gmail.com>
2023-11-28 22:12:55 +0100

    model is able to train, need to add tracker and logger and refactor the model, also need save_to and load_from to save model weights

--- src/dataset/emotions_dataset.py
+++ src/dataset/emotions_dataset.py
@@ -1,9 +1,16 @@
 import re
+import nltk
+from nltk.corpus import stopwords
+
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
-import nltk
-from nltk.corpus import stopwords
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext import transforms as T
+
+import config
+
+nltk.data.path.append("..\\data\\nltk")
 
 
 class EmotionsDataset(Dataset):
@@ -28,11 +35,36 @@ class EmotionsDataset(Dataset):
 
     def __init__(self, csv_file):
         self.data = pd.read_csv(csv_file)
+        self.vocab = self.build_vocab()
 
     def __len__(self):
         return len(self.data)
 
-    def clean_text(self, text: str, max_length: int):
+    def build_vocab(self):
+        """
+        Builds a vocabulary from the dataset.
+
+        Returns:
+            torchtext.vocab.Vocab: The vocabulary object.
+        """
+
+        vocab = build_vocab_from_iterator(
+            self.data["Text"]
+            .apply(self.clean_text)
+            .apply(self.remove_stop_words)
+            .apply(self.tokenize)
+            .apply(self.lemmatize),
+            min_freq=2,
+            specials=["<unk>", "<pad>"],
+            special_first=True,
+            max_tokens=config.MAX_WORDS,
+        )
+
+        vocab.set_default_index(vocab["<unk>"])
+
+        return vocab
+
+    def clean_text(self, text: str, max_length: int = None):
         """
         Cleans the given text by removing punctuation, sequences of spaces, newline characters,
         and fixing the length to the specified maximum length.
@@ -49,12 +81,13 @@ class EmotionsDataset(Dataset):
         text = re.sub(r"[^\w\s]", "", text)
         # Remove sequences of spaces
         text = re.sub(r"\s+", " ", text)
-        # Fix the length
-        text = text[:max_length].ljust(max_length)
         # Remove newline characters
         text = text.replace("\n", "")
         # Convert to lowercase
         text = text.lower()
+
+        if max_length is not None:
+            text = text[:max_length]
 
         return text
 
@@ -74,7 +107,7 @@ class EmotionsDataset(Dataset):
         cleaned_text = " ".join(filtered_text)
         return cleaned_text
 
-    def tokenize(self, text: str, char_based: bool = False):
+    def tokenize(self, text: str, char_based: bool = False, fix_length: int = None):
         """
         Tokenizes the given text into a list of tokens.
 
@@ -82,17 +115,45 @@ class EmotionsDataset(Dataset):
             text (str): The input text to be tokenized.
             char_based (bool, optional): If True, tokenizes the text into individual characters.
                 If False (default), tokenizes the text into words using space as the delimiter.
+            fix_length (int, optional): The maximum length of the tokenized list. If specified,
+                the list will be padded or truncated to this length.
 
         Returns:
             list: A list of tokens.
         """
 
         if char_based:
-            return list(text)
-        return text.split(" ")
+            tokens = list(text)
+        else:
+            tokens = text.split(" ")
+
+        if fix_length is not None:
+            if len(tokens) < fix_length:
+                tokens = ["<pad>"] * (fix_length - len(tokens)) + tokens
+            else:
+                tokens = tokens[:fix_length]
+
+        return tokens
 
     def lemmatize(self, text: [str]):
         return text  # TODO: Implement
+
+    def turn_to_indices(self, text: [str], vocab):
+        """
+        Turns the given text into a list of indices based on the given vocabulary.
+
+        Args:
+            text (list): The text to be converted.
+            vocab (torchtext.vocab.Vocab): The vocabulary to use for conversion.
+
+        Returns:
+            list: A list of indices.
+        """
+        text_transform = T.Sequential(
+            T.VocabTransform(vocab),
+            T.ToTensor(),
+        )
+        return text_transform(text)
 
     def encode_emotion(self, emotion: int, num_emotions: int):
         """
@@ -106,7 +167,7 @@ class EmotionsDataset(Dataset):
             torch.Tensor: The one-hot encoded emotion vector.
         """
 
-        emotion_one_hot = torch.zeros(num_emotions)
+        emotion_one_hot = torch.zeros(num_emotions)  # , dtype=torch.long
         emotion_one_hot[emotion] = 1
         return emotion_one_hot
 
@@ -115,21 +176,21 @@ class EmotionsDataset(Dataset):
         sample = self.data.iloc[idx]
 
         # Clean the text
-        text = self.clean_text(sample["Text"], max_length=100)
+        text = self.clean_text(sample["Text"])
         text = self.remove_stop_words(text)
-        text = self.tokenize(text, char_based=False)
+        text = self.tokenize(text, char_based=False, fix_length=config.WORDS_PER_SAMPLE)
         text = self.lemmatize(text)
+        text = self.turn_to_indices(text, self.vocab)
 
         # Encode emotion to one-hot vector
         emotions = self.data["Emotion"].unique()
-        num_emotions = len(emotions)
         emotion = sample["Emotion"]
         emotion = list(emotions).index(emotion)
-        emotion_one_hot = self.encode_emotion(emotion, num_emotions)
+        emotion_one_hot = self.encode_emotion(emotion, config.NUM_EMOTIONS)
 
         # Return the sample as a dictionary or tuple
         return {
             "text": text,
             "emotion": emotion_one_hot,
-            "emotion_text": sample["Emotion"],
+            # "emotion_text": sample["Emotion"],
         }
--- src/main.py
+++ src/main.py
@@ -1,9 +1,102 @@
 import torch
-from torch.utils.data import DataLoader
-from src import config
+from torch import nn
+from torch.utils.data import DataLoader, random_split
 
-from src.dataset.emotions_dataset import EmotionsDataset
+import config
+from dataset.emotions_dataset import EmotionsDataset
 
-# Create a dataset and dataloader
+# Create a dataset
 dataset = EmotionsDataset("../data/emotions-in-text/Emotion_final.csv")
-dataloader = DataLoader(dataset, batch_size=config.BATCH_SIZE, shuffle=True)
+
+# Split the dataset into train and eval
+train_size = int(len(dataset) * config.TRAIN_RATIO)
+eval_size = len(dataset) - train_size
+train_dataset, eval_dataset = random_split(dataset, [train_size, eval_size])
+
+dataset.build_vocab()
+
+# Create dataloaders for train and eval
+train_dataloader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
+eval_dataloader = DataLoader(eval_dataset, batch_size=config.BATCH_SIZE, shuffle=False)
+
+
+class EmotionsClassifier(nn.Module):
+    def __init__(self, num_words, embedding_dim, num_emotions):
+        super().__init__()
+
+        self.embedding = nn.Embedding(num_words, embedding_dim)
+        self.lstm = nn.LSTM(
+            embedding_dim,
+            100,
+            # bidirectional=True,
+            batch_first=True,
+            # dropout=0.2
+        )
+        self.linear = nn.Linear(100, num_emotions)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.lstm(x)
+        x = self.linear(x[1][0][0])
+        # x[0] is the output of the LSTM, x[1] is the hidden state of the LSTM,
+        # contains the last hidden state and last cell state
+        x = self.softmax(x)
+        return x
+
+
+model = EmotionsClassifier(config.MAX_WORDS, config.EMBEDDING_DIM, config.NUM_EMOTIONS)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=config.LEARNING_RATE)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, patience=10, verbose=True
+)
+
+for epoch in range(config.EPOCHS):
+    # Train the model
+    model.train()
+    i = 0
+    running_loss = 0.0
+    for batch in train_dataloader:
+        # Get the inputs and labels
+        inputs = batch["text"]
+        labels = batch["emotion"]
+
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Forward pass, backward pass, and optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # Print statistics
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100}")
+            running_loss = 0.0
+        i += 1
+
+    # Evaluate the model
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch in eval_dataloader:
+            # Get the inputs and labels
+            inputs = batch["text"]
+            labels = batch["emotion"]
+
+            # Predict and count
+            outputs = model(inputs)
+            total += labels.size(0)
+            correct += (
+                (torch.argmax(outputs, dim=1) == torch.argmax(labels, dim=1))
+                .sum()
+                .item()
+            )
+
+    scheduler.step(correct / total)
+
+    print(f"Accuracy: {100 * correct / total}%")
